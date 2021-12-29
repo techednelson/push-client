0161e3f042a0c1f41d079f72ba1bc8a8a4c077dc
Nelson Sandoval <techednelson@gmail.com>
2021-12-29 13:29:50 +0200

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,7 @@
-// Scripts for firebase and firebase messaging
 // eslint-disable-next-line no-undef
-importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 // eslint-disable-next-line no-undef
-importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
 // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
