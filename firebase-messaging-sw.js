ca2dd1bf6fbb0a460511872ff62ff0c3ade3c42a
Nelson Sandoval <techednelson@gmail.com>
2021-12-29 04:35:53 +0200

    Updates

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line no-undef
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+// eslint-disable-next-line no-undef
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+
+// Initialize the Firebase app in the service worker by passing the generated config
+const firebaseConfig = {
+  apiKey: "AIzaSyCtFMtwRSc-YetlNoG6NIH4WknPnKh76Ls",
+  authDomain: "notification-system-7d1a2.firebaseapp.com",
+  projectId: "notification-system-7d1a2",
+  storageBucket: "notification-system-7d1a2.appspot.com",
+  messagingSenderId: "327795312923",
+  appId: "1:327795312923:web:557079104d0d21f71b6bbe"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Received background message ', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  // eslint-disable-next-line no-restricted-globals
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
